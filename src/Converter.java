130a2ac6482122aeaf98993b2afa3e5df77b002b
Панков Денис <Pankkovv@yandex.ru>
2022-09-18 16:21:48 +0300

    Создал класс конвертора шагов

--- src/Converter.java
+++ src/Converter.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Converter {
+public class Converter {
+    double oneStepKM = 0.00075;
+    double oneStepCal = 0.05;
+    double conStepKM;
+    double conStepCal;
+
+
+    double stepKM(int count){
+        return conStepKM = oneStepKM * count;
+    }
+
+    double stepCal(int count){
+        return conStepCal = oneStepCal * count;
+    }
 }
