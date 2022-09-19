842baefee762b77d0e859f34da20d4b7165a728c
Панков Денис <Pankkovv@yandex.ru>
2022-09-19 15:18:04 +0300

    Добавил Converter обратно

---
+++ src/Converter.java
@@ -0,0 +1,41 @@
+[33mcommit 3c4e52e9935be4600dc80ed9b93b9dedb16a48dd[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Панков Денис <Pankkovv@yandex.ru>
+Date:   Mon Sep 19 13:04:03 2022 +0300
+
+    Исправил вывод статистики за последние дни
+
+[33mcommit 51535556f5e5c8160519fcf80ad3f3e1c99c8404[m
+Author: Панков Денис <Pankkovv@yandex.ru>
+Date:   Mon Sep 19 13:03:22 2022 +0300
+
+    Внес изменения после ревью
+
+[33mcommit a6eb479747dfdb7d81d903a59358ee7fb71baa8a[m
+Author: Панков Денис <Pankkovv@yandex.ru>
+Date:   Sun Sep 18 16:31:05 2022 +0300
+
+    1 ТЗ
+
+[33mcommit e3e683d4b3c5c5c66211ec24fdabd947b2427e98[m
+Author: Панков Денис <Pankkovv@yandex.ru>
+Date:   Sun Sep 18 16:22:27 2022 +0300
+
+    Создал класс основной функциональности
+
+[33mcommit 130a2ac6482122aeaf98993b2afa3e5df77b002b[m
+Author: Панков Денис <Pankkovv@yandex.ru>
+Date:   Sun Sep 18 16:21:48 2022 +0300
+
+    Создал класс конвертора шагов
+
+[33mcommit 117f8469ab1645633ceb78900d03cfe05d38eb07[m
+Author: Панков Денис <Pankkovv@yandex.ru>
+Date:   Sun Sep 18 16:20:13 2022 +0300
+
+    Прописал основную логику программы
+
+[33mcommit 2ac166f830f325392816e584a4666dd37c496987[m
+Author: Панков Денис <pankkovv@yandex.ru>
+Date:   Sat Sep 17 12:26:22 2022 +0300
+
+    Initial commit
